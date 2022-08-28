7b6996eea2af30d9536588e623649e5771652940
Lacedaimon95 <svetozar95@gmail.com>
2022-08-29 01:42:02 +0200

    #3

--- MyApp/src/app/books/books.component.ts
+++ MyApp/src/app/books/books.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 interface Book {
-  name: string,
-  author: string,
-  image: string
+  name: string;
+  author: string;
+  image: string;
+  amount: number;
 }
 
 @Component({
@@ -18,13 +19,14 @@ export class BooksComponent implements OnInit {
     {
       name: "Clean Code",
       author: "Robert C Martin",
-      image: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UY218_.jpg"
+      image: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UY218_.jpg",
+      amount: 110,
     },
     {
       name: "The Pragmatic Programmer",
       author: " Andrew Hunt",
-      image: "https://images-na.ssl-images-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg"
-  
+      image: "https://images-na.ssl-images-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg",
+      amount: 80,
     }
 
   ]
