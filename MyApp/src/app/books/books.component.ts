b16db9fc01e28c2da8ba95ea7885994de5418d93
Lacedaimon95 <svetozar95@gmail.com>
2022-08-30 02:00:59 +0200

    #4

--- MyApp/src/app/books/books.component.ts
+++ MyApp/src/app/books/books.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Book {
-  name: string;
-  author: string;
-  image: string;
-  amount: number;
-}
+import { Book } from '../types/Book';
 
 @Component({
   selector: 'app-books',
@@ -18,22 +12,38 @@ export class BooksComponent implements OnInit {
   books: Book[] = [
     {
       name: "Clean Code",
-      author: "Robert C Martin",
+      author: "Robert C. Martin",
       image: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UY218_.jpg",
-      amount: 110,
+      amount: 110.55,
     },
     {
       name: "The Pragmatic Programmer",
       author: " Andrew Hunt",
       image: "https://images-na.ssl-images-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg",
-      amount: 80,
-    }
+      amount: 80.44,
+    },
+    {
+      name: "The Art of Computer Programming",
+      author: "Donald E. Knuth",
+      image: "https://images-na.ssl-images-amazon.com/images/I/41msKv2oN9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      amount: 65.12,
+    },
+    {
+      name: "You Don't Know JS Yet: Get Started",
+      author: "Kyle Simpson",
+      image: "https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+      amount: 24.74,
+    },
+  ];
 
-  ]
 
-  
-
+  cart: Book[] = [];
   isShowing: boolean = true;
+
+
+  addToCart(value:Book){
+    console.log(value);
+  }
 
 
   
---
+++ MyApp/src/app/types/Book.ts
@@ -0,0 +1,11 @@
+export type Book = {
+    name: string,
+    author: string,
+    image: string,
+    amount: number,
+  }
+
+
+
+  // u type smo definisali one ts tipove za varijable koje treba da prosledimo
+  // da bi mogli da koristimo prosledjene propertije
